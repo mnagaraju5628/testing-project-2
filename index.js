900e52b744f732baeb91f17babc08e9e2447e317
mnagaraju5628 <124573100+mnagaraju5628@users.noreply.github.com>
2026-02-01 08:38:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,15 @@ app.get('/', function (req, res) {
 app.get('/will', function (req, res) {
     res.send('{ "response": "Welcome" }');
 });
+
 app.get('/ready', function (req, res) {
     res.send('{ "response": " Great!, It works!" }');
 });
-app.listen(process.env.PORT || 4000);
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
 module.exports = app;
